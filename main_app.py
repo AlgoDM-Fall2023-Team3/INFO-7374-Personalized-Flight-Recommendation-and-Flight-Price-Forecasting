0ba6fa9cc01db7f89f0d20c005034f97ec000e67
Ashwini Khedkar <ashwinikhedkar@Ashwinis-MacBook-Air.local>
2023-12-15 22:21:54 -0500

    app

--- main_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Recommend Airline",
-    page_icon="👋",
-)
-st.title('Personalized Flight Recommendation System and Price Forecasting')
-st.image('flight_recommendation-4.png')
